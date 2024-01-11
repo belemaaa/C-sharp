1f31a4e63a8c1ace40a24e2d26115ae1cee469ea
belemaaa <graceitamunoala@gmail.com>
2024-01-11 15:29:52 +0100

    loops

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // while loop
         Console.WriteLine("Enter your target value: ");
         int targetValue = int.Parse(Console.ReadLine());
 
@@ -13,5 +14,27 @@ class Program
             count += 1;
         }
 
+        // do while loop
+        int[] Numbers = new int[3];
+        Numbers[0] = 1;
+        Numbers[1] = 2;
+        Numbers[2] = 3;
+
+        int i = 0;
+        do{
+            Console.WriteLine(Numbers[i]);
+            i++;
+        }
+        while (i < Numbers.Length);
+
+        // for loop
+        for (int j = 0; j < Numbers.Length; j++){
+            Console.WriteLine("\n" + Numbers[j]);
+        }
+
+        // foreach loop
+        foreach(int number in Numbers){
+            Console.WriteLine(number);
+        }
     }
 }
