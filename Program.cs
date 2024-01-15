9284a86bbde295fdaba318668ee4c556e4c526a6
belemaaa <graceitamunoala@gmail.com>
2024-01-15 08:15:31 +0100

    methods

--- Program.cs
+++ Program.cs
@@ -2,39 +2,23 @@
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        // while loop
-        Console.WriteLine("Enter your target value: ");
-        int targetValue = int.Parse(Console.ReadLine());
+        Program.EvenNumbers(5);
 
+        Program animal = new Program();
+        animal.Animal("Cat");
+    }
+
+    public static void EvenNumbers(int Target){
         int count = 0;
-        while (count <= targetValue){
-            Console.WriteLine("Count {0}", count);
-            count += 1;
+        while (count <= Target){
+            Console.WriteLine(count);
+            count += 2;
         }
+    }
 
-        // do while loop
-        int[] Numbers = new int[3];
-        Numbers[0] = 1;
-        Numbers[1] = 2;
-        Numbers[2] = 3;
-
-        int i = 0;
-        do{
-            Console.WriteLine(Numbers[i]);
-            i++;
-        }
-        while (i < Numbers.Length);
-
-        // for loop
-        for (int j = 0; j < Numbers.Length; j++){
-            Console.WriteLine("\n" + Numbers[j]);
-        }
-
-        // foreach loop
-        foreach(int number in Numbers){
-            Console.WriteLine(number);
-        }
+    public void Animal(string type){
+        Console.WriteLine("This is a {0}", type);
     }
 }
--- obj/Debug/net8.0/CSHARPTUTORIAL.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSHARPTUTORIAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSHARPTUTORIAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b603939c4957858f755499fdac38a03f2b300b33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f31a4e63a8c1ace40a24e2d26115ae1cee469ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSHARPTUTORIAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSHARPTUTORIAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
