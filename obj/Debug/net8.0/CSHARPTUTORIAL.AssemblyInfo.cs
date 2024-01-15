0b53d42bccf728b1588fe39c65af3aec18f33640
belemaaa <graceitamunoala@gmail.com>
2024-01-15 15:08:30 +0100

    parameter arrays

--- obj/Debug/net8.0/CSHARPTUTORIAL.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSHARPTUTORIAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSHARPTUTORIAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ea6b20e61f0af0eb48a3de499e7e3ef93c8029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e87b87cb541242fc7e523619b393452ddb7ade")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSHARPTUTORIAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSHARPTUTORIAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
