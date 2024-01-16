4be1717508c081dc6a5fcc1204c1c2233ef97bd7
belemaaa <graceitamunoala@gmail.com>
2024-01-16 18:24:17 +0100

    defining a constructor

--- Classes.cs
+++ Classes.cs
@@ -1,8 +1,22 @@
 namespace CSHARPTUTORIAL;
 
+
+class Customer
+{
+    string _firstname;
+    string _lastname;
+
+    public Customer(string firstName, string lastName)
+    {
+        this._firstname = firstName;
+        this._firstname = lastName;
+    }
+}
+
 public class Classes
 {
-    public static void ClasseslESSON(){
-        
+    public static void ClassesLesson()
+    {
+
     }
 }
