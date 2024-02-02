5fac68c14d48ab9285c36ef72ea8baa86e173da2
belemaaa <graceitamunoala@gmail.com>
2024-02-02 09:23:29 +0100

    generic classes

--- Generics.cs
+++ Generics.cs
@@ -4,14 +4,14 @@ public class Generics
 {
     public static void GenericsOps()
     {
-        bool equal = Calculator.AreEqual<string>("grace", "grace");
+        bool equal = Calculator<string>.AreEqual("grace", "grace");
     }
 
-    public class Calculator
+    public class Calculator<T>
     {
         // generics make yoir code type independent
         // it allows you specify the data type to be sent to the method during method call
-        public static bool AreEqual<T>(T value1, T value2)
+        public static bool AreEqual(T value1, T value2)
         {
             return value1.Equals(value2);
         }
